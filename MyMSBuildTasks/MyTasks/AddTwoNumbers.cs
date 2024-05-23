3844c48da8aac14ab3210c7d27f34ad2ffe7b5e1
Stuart Mathews <Stuart.Mathews@citrix.com>
2024-05-23 17:37:12 +0100

    add ability to call custom msbuild tasks!

---
+++ MyMSBuildTasks/MyTasks/AddTwoNumbers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTasks
+{
+    public class AddTwoNumbers : ITask
+    {
+
+        [Required]
+        public double Number1 { get; set; }
+
+        [Required]
+        public double Number2 { get; set; }
+
+        // pass a value back to MS build
+
+        [Output]
+        public double Result { get; set; }
+
+        public IBuildEngine BuildEngine { get;set; }
+
+        public ITaskHost HostObject { get;set; }
+
+        public bool Execute()
+        {
+            Result = Number1 + Number2;
+
+            var args = new BuildMessageEventArgs("Added two numbers ", "Add", nameof(AddTwoNumbers), MessageImportance.High);
+            BuildEngine.LogMessageEvent(args);
+
+            return true;
+        }
+    }
+}
---
+++ MyMSBuildTasks/MyTasks/MultiplyTwoNumbers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTasks
+{
+    public class MultiplyTwoNumbers : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public double Number1 { get; set; }
+
+        [Required]
+        public double Number2 { get; set; }
+
+        // pass a value back to MS build
+
+        [Output]
+        public double Result { get; set; }
+
+        public override bool Execute()
+        {
+            Result = Number1 * Number2;
+
+            Log.LogMessage(MessageImportance.High, "Multiplied two numbers", null);
+            
+            return true;
+        }
+    }
+}
